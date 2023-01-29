4d0a53f89708adf665753a02a9b6f041d15245dc
Marcel van der Heide <marcel.van.der.heide@fh-erfurt.de>
2023-01-29 20:44:31 +0100

    add debug methods + cleanup -> check full commit message
    - Always release camera access
    - Call new grid.json method and fetch result
    - Add difficulty to output json
    - Use demo image for readyDebug

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
 import requests
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Response
 from pydantic import BaseModel
-# from function import takePicture  # , function
 import logging
 import json
-from function import takePicture
+import pathlib
+import cv2
+from src import grid
 
 # logging
-
 LOG = "logging_data.log"
 logging.basicConfig(filename=LOG, filemode="w", level=logging.DEBUG)
 
@@ -16,51 +16,96 @@ console = logging.StreamHandler()
 console.setLevel(logging.ERROR)
 logging.getLogger("").addHandler(console)
 
+# configuration
+saturation = '1.2'
+grid_buffer = '1'
+human_color = 'red'
+robot_color = 'green'
+image_resize = '500'
+camera_id = 1
+
+
 class Difficulty(BaseModel):
-  difficulty: int
+    difficulty: int
+
 
 app = FastAPI()
 
-#########################
-# 6 0  0  0  0  0  0  0 #
-# 5 0  0  0  0  0  0  0 #
-# 4 0  0  0  0  0  0  0 #
-# 3 h  0  0  r  0  0  0 #
-# 2 h  r  r  h  r  0  r #
-# 1 h  h  h  r  h  r  r #
-# # 1  2  3  4  5  6  7 #
-
-# h = Human Player | r = Robot Player | 0 = empty space
-
-
-# }
 
 @app.get("/")
 async def home():
-  return {"Hello World"}
+    return {"Hello World"}
 
 
 @app.post("/ready")
 async def ready(difficulty: Difficulty):
-    if difficulty != 0:
-        Difficulty = difficulty
-        takePicture()
-        logging.info(str(Difficulty))
+    # returns the found and analyzed grid as json
+    try:
+        grid_json = json.loads(analyze_grid())
+        grid_json.update({'Difficulty': difficulty.difficulty})
+    except:
+        grid_json = json.dumps({'error': 'Error analyzing grid'})
 
-        addMessage = '{ "Column1": {"Row1":"h", "Row2":"0", "Row3":"0", "Row4":"0", "Row5":"0", "Row6":"0"}, "Column2": {' \
-                     '"Row1":"0", "Row2":"0", "Row3":"0", "Row4":"0", "Row5":"0", "Row6":"0"}, "Column3": {"Row1":"0", ' \
-                     '"Row2":"0", "Row3":"0", "Row4":"0", "Row5":"0", "Row6":"0"}, "Column4": {"Row1":"0", "Row2":"0", ' \
-                     '"Row3":"0", "Row4":"0", "Row5":"0", "Row6":"0"}, "Column5": {"Row1":"0", "Row2":"0", "Row3":"0", ' \
-                     '"Row4":"0", "Row5":"0", "Row6":"0"}, "Column6": {"Row1":"h", "Row2":"h", "Row3":"h", "Row4":"0", ' \
-                     '"Row5":"0", "Row6":"0"}, "Column7": {"Row1":"r", "Row2":"r", "Row3":"0", "Row4":"0", "Row5":"0", ' \
-                     '"Row6":"0"}, "Difficulty": Difficulty.difficulty}'
+    message = json.dumps(grid_json, indent=4)
+    r = requests.post(f"http://localhost:8093/updateBoard", json=message)
+    return Response(r.text)
 
-        addMessage = json.loads(addMessage)
-        r = requests.post(f"http://localhost:8093/updateBoard", json=addMessage)
-        # logging.info(f"Status Code: {r.status_code}, Response: {r.text}")
-        return {"Response": r.text}  # Post http://localhost:8093/updateBoard
-    else:
-        return {"no difficulty"}
+
+@app.post("/readyDebug")
+async def ready_debug(difficulty: Difficulty):
+    # returns the found and analyzed grid as json
+    try:
+        grid_json = json.loads(analyze_grid(False))
+        grid_json.update({'Difficulty': difficulty.difficulty})
+    except:
+        grid_json = json.dumps({'error': 'Error analyzing grid'})
+
+    message = json.dumps(grid_json, indent=4)
+    return Response(message)
+
+
+@app.post("/readyDebugStatic")
+async def ready_debug_static(difficulty: Difficulty):
+    add_message = {
+        "Column1": {"Row1": "h", "Row2": "0", "Row3": "0", "Row4": "0", "Row5": "0", "Row6": "0"},
+        "Column2": {"Row1": "0", "Row2": "0", "Row3": "0", "Row4": "0", "Row5": "0", "Row6": "0"},
+        "Column3": {"Row1": "0", "Row2": "0", "Row3": "0", "Row4": "0", "Row5": "0", "Row6": "0"},
+        "Column4": {"Row1": "0", "Row2": "0", "Row3": "0", "Row4": "0", "Row5": "0", "Row6": "0"},
+        "Column5": {"Row1": "0", "Row2": "0", "Row3": "0", "Row4": "0", "Row5": "0", "Row6": "0"},
+        "Column6": {"Row1": "h", "Row2": "h", "Row3": "h", "Row4": "0", "Row5": "0", "Row6": "0"},
+        "Column7": {"Row1": "r", "Row2": "r", "Row3": "0", "Row4": "0", "Row5": "0", "Row6": "0"},
+        "Difficulty": difficulty.difficulty}
+
+    message = json.dumps(add_message, indent=4)
+    return Response(message)
+
+
+def analyze_grid(take_image: bool = True):
+    img_path = str(pathlib.Path(__file__).resolve().parent.parent) + '/assets/image_demo.jpg'
+
+    global saturation, saturation, grid_buffer, human_color, robot_color, image_resize, camera_id
+
+    # Take image from camera if needed
+    if take_image:
+        cam = None
+        try:
+            img_path = '/tmp/camImage.jpg'
+            cam = cv2.VideoCapture(camera_id)
+            ret, image = cam.read()
+            cv2.imwrite(img_path, image)
+        finally:
+            if cam is not None:
+                cam.release()
+
+
+    return grid.json([
+        img_path,
+        '--hc=' + human_color,
+        '--rc=' + robot_color,
+        '--bb=' + grid_buffer,
+        '--resize=' + image_resize,
+        '--saturation=' + saturation,
+    ])
 
 # PORT Bildverarbeitung: 8090
 # PORT Spielalgorithmus: 8093
