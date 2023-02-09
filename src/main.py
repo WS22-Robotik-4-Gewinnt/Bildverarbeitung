5fe3cadf946c1dd15c56b09c3135500309a0c8ff
Frieder Ullmann <friederullmann@yahoo.de>
2023-02-09 18:01:03 +0100

    change main

--- src/main.py
+++ src/main.py
@@ -12,7 +12,7 @@ LOG = "logging_data.log"
 logging.basicConfig(filename=LOG, filemode="w", level=logging.DEBUG)
 
 
-target_ip = "http://172.17.0.1"
+
 # console handler
 console = logging.StreamHandler()
 console.setLevel(logging.ERROR)
@@ -49,7 +49,8 @@ async def ready(difficulty: Difficulty):
         grid_json = json.dumps({'error': 'Error analyzing grid'})
 
     message = json.dumps(grid_json, indent=4)
-    r = requests.post(target_ip + ":8093/updateBoard", json=message)
+    print(str(message))
+    r = requests.post(f"http://172.17.0.1:8093/updateBoard", json=message)
     return Response(r.text)
 
 
